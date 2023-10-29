6f932d53bc68fc366be5cd22eed959fac945b258
Jordan Fox <jordan.fox@canstar.com.au>
2023-10-29 17:54:51 +1000

    created posts table

--- db.go
+++ db.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/joho/godotenv"
@@ -9,10 +10,10 @@ import (
 	"gorm.io/gorm"
 )
 
-type Product struct {
+type Post struct {
 	gorm.Model
-	Code  string
-	Price uint
+	Text  string
+	Title string `gorm:unique`
 }
 
 func db() {
@@ -30,22 +31,33 @@ func db() {
 	}
 
 	// Migrate the schema
-	db.AutoMigrate(&Product{})
+	db.AutoMigrate(&Post{})
 
-	// Create
-	db.Create(&Product{Code: "D42", Price: 100})
+	post := Post{Text: "hello world", Title: "post 1"}
+	result := db.Create(&post)
+	if result.Error != nil {
+		log.Fatalf("Failed to create post: %v", result.Error)
+	}
 
 	// Read
-	var product Product
-	db.First(&product, 1)                 // find product with integer primary key
-	db.First(&product, "code = ?", "D42") // find product with code D42
+	var firstPost Post
 
-	// Update - update product's price to 200
-	db.Model(&product).Update("Price", 200)
-	// Update - update multiple fields
-	db.Model(&product).Updates(Product{Price: 200, Code: "F42"}) // non-zero fields
-	db.Model(&product).Updates(map[string]interface{}{"Price": 200, "Code": "F42"})
+	if err := db.First(&firstPost).Error; err != nil {
+		log.Fatalf("Failed to read the first post: %v", err)
+	} else {
+		fmt.Printf("Title of the first post: %s\n", firstPost.Title)
+	}
+	// Read
+	// var product Post
+	// db.First(&product, 1)                 // find product with integer primary key
+	// db.First(&product, "code = ?", "D42") // find product with code D42
 
-	// Delete - delete product
-	db.Delete(&product, 1)
+	// // Update - update product's price to 200
+	// db.Model(&product).Update("Price", 200)
+	// // Update - update multiple fields
+	// db.Model(&product).Updates(Post{Text: "hello world", Title: "post 1"}) // non-zero fields
+	// db.Model(&product).Updates(map[string]interface{}{"Price": 200, "Code": "F42"})
+
+	// // Delete - delete product
+	// db.Delete(&product, 1)
 }
